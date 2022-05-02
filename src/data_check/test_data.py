f7ea512384f758acd08d0ed5f4f799aa9bb320ff
waichung <leewaichung5@gmail.com>
2022-05-02 16:17:46 +0100

    implement config and code for data_check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    """
+    Check that the size of the dataset is reasonable
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Check the price data is within the defined range
+    """
+    assert all(data['price'].between(min_price, max_price))
