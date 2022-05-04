1fd76799a7d32a5c705d87545efbffc34e8eb640
wclee-uwe <Wai10.Lee@uwe.ac.uk>
2022-05-04 21:46:55 +0100

    fix: fixed failure due dataset not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,11 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # drop rows in the dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(
+        40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
 
     artifact = wandb.Artifact(
